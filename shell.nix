a145d978ce8e3b9bed23923940280744e823a8d1
ernestkz <ernestkuznecovs@gmail.com>
2022-02-13 16:29:40 +0000

    Dealing with figures.

--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-let
-  python-with-my-packages = pkgs.python3.withPackages (p: with p; [
-    numpy
-    sympy
-    matplotlib
-  ]);
-in
-python-with-my-packages.env 
