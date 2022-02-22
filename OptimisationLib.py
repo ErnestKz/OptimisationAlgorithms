19fa262969a13290ddd44d70243e5a09f38fa1ce
ernestkz <ernestkuznecovs@gmail.com>
2022-02-22 18:28:12 +0000

    Move code to single file.

--- OptimisationLib.py
+++ OptimisationLib.py
@@ -1,3 +1,130 @@
+import matplotlib as mpl
+mpl.rcParams['figure.dpi'] = 200
+mpl.rcParams['figure.facecolor'] = '1'
+import matplotlib.pyplot as plt
+
+from sympy import diff, lambdify, symbols, init_printing, Max, Abs
+init_printing(use_unicode=False)
+
+
+x, y = symbols('x y', real=True)
+
+f1 = 3 * (x-9)**4 + 5 * (y-9)**2
+df1dx = diff(f1, x)
+df1dy = diff(f1, y)
+
+f2 = Max(x-9 ,0) + 5 * Abs(y-9)
+df2dx = diff(f2, x)
+df2dy = diff(f2, y)
+
+df2dxl = lambdify([x, y], df2dx, modules="numpy")
+df2dyl = lambdify([x, y], df2dy, modules="numpy")
+f2l = lambdify([x, y], f2, modules="numpy")
+print(df2dxl(8, 1))
+print(df2dyl(8, 8))
+
+df1dxl = lambdify([x, y], df1dx, modules="numpy")
+df1dyl = lambdify([x, y], df1dy, modules="numpy")
+f1l = lambdify([x, y], f1, modules="numpy")
+
+
+def polyak(x0, f, f_star, eps=0.0001, iters=50):
+    x = x0
+    X = [x]
+    dfs = f.dfs
+    f = f.f
+    Y = [f(*x)]
+
+    # now need to return x, y, dx
+    for _ in range(iters):
+        fdif = f(*x) - f_star
+        df_squared_sum = np.sum(np.array([df(*x)**2 for df in dfs]))
+        alpha = fdif / (df_squared_sum + eps)
+        x = x - alpha * np.array([df(*x) for df in dfs])
+
+        X += [x]
+        Y += [f(*x)]
+        # can  calculate by taking difference of the previous X
+        
+    return X, Y
+
+
+def adagrad(dfs,
+            x0,   # this is a vector, the input to f/df
+            i_max=50):
+    
+    x_history = [x0]                             # keep track of history of xi's
+    for _ in range(i_max):
+        subgradient_step_vector = []             # each partial gets its own step
+
+        
+        for df in dfs:
+            subgradient_step = adagrad_partial_alpha(df, x_history, alpha0=0.1, eps=0.001) * df(x[-1])
+            subgradient_step_vector += [ subgradient_step ]
+        x = x - subgradient_step_vector; x_history += [x]
+    return x
+
+
+def rmsprop(x0, f, alpha0, beta, eps, iters_max):
+    sum = np.array([0.0, 0.0]) ; x = x0 ; alpha = alpha0
+
+    dfs = f.dfs
+    f = f.f
+    
+    X = [x]
+    Y = [f(*x)]
+    for _ in range(iters_max):
+      x = x - (alpha * np.array([df(*x) for df in dfs]))
+      sum = beta * sum + (1 - beta) * np.array([float(df(*x)**2) for df in dfs]) # would be nice to write out the formula for this
+      alpha = alpha0 / (np.sqrt(sum) + eps)
+      X += [x]
+      Y += [f(*x)]
+    return X, Y
+
+def heavy_ball(x0, f, alpha, beta, iters_max):
+    dfs = f.dfs
+    f = f.f
+    x = x0; z = np.array([0, 0]);
+
+    X = [x]
+    Y = [f(*x)]
+    for _ in range(iters_max):
+        z = beta * z + alpha * np.array([float(df(*x)) for df in dfs])
+        x = x - z
+
+        X += [x]
+        Y += [f(*x)]
+        
+    return X, Y
+
+def adam(x0, f, eps, beta1, beta2, alpha, iters_max):
+    x = x0; m = np.array([0.0, 0.0]); v = np.array([0.0, 0.0])
+
+    dfs = f.dfs
+    f = f.f
+    X=[x]
+    Y=[f(*x)]
+    
+    for k in range(iters_max):
+        i = k + 1
+        # m = beta1 * m + np.sum((1 - beta1) * np.array([float(df(*x)) for df in dfs]))
+
+        m = beta1 * m + (1 - beta1) * np.array([float(df(*x)) for df in dfs])
+        v = beta2 * v + (1 - beta2) * np.array([float((df(*x)**2)) for df in dfs])
+        
+        mhat = (m / (1 - beta1**i))   # what are these doing?
+        vhat = (v / (1 - beta2**i))
+        # x = x - alpha * np.array([(mhat / (np.sqrt(vhati) + eps)) for vhati in vhat])
+        x = x - alpha * (mhat / (np.sqrt(vhat) + eps))
+
+        
+        X += [x]
+        Y += [f(*x)]
+
+    return X,Y
+
+
+
 def finite_diff(f, x, delta=0.01):
     return (f(x) - f(x - delta)) / delta
 
@@ -83,6 +210,17 @@ def runOptimisations(optAlg, inputs):
     return outputs
 
 
+f2 = Fn(f=f2l, dfs=[ df2dxl, df2dyl ], name="blah")
+
+
+# polyak_alg = OptAlg(f=polyak, name="polyak")
+# inputs= mkInput(
+#     x0=[21, 21],
+#     f=f2,
+#     f_star=0,
+#     eps=0.0001,
+#     iters=60,
+# )
 # out = runOptimisations(optAlg=polyak_alg,
 #                        inputs=inputs
 #                        )
@@ -90,7 +228,6 @@ def runOptimisations(optAlg, inputs):
 
 
 # rms_alg = OptAlg(f=rmsprop, name="rmsprop")
-
 # inputs= mkInput(
 #     x0=[35,-20],
 #     f=f2,
@@ -99,14 +236,12 @@ def runOptimisations(optAlg, inputs):
 #     eps=0.0001,
 #     iters_max=70
 # )
-
 # out = runOptimisations(optAlg=rms_alg,
 #                        inputs=inputs
 #                        )
 
 
 # heavy_alg = OptAlg(f=heavy_ball, name="heavy ball")
-
 # inputs= mkInput(
 #     x0=[35,-20],
 #     f=f2,
@@ -114,7 +249,72 @@ def runOptimisations(optAlg, inputs):
 #     beta=0.9,
 #     iters_max=70
 # )
-
 # out = runOptimisations(optAlg=heavy_alg,
 #                        inputs=inputs
 #                        )
+
+
+
+# f2 = Fn(f=f2l, dfs=[ df2dxl, df2dyl ], name="blah")
+# adam_alg = OptAlg(f=adam, name="adam")
+
+# inputs= mkInput(
+#     x0=[35,-20],
+#     f=f2,
+#     eps=0.0001,
+#     beta1=0.9,
+#     beta2=0.9,
+#     alpha=0.4,
+#     iters_max=70
+# )
+
+# out = runOptimisations(optAlg=adam_alg,
+#                        inputs=inputs
+#                        )
+# print(out[0]['X'])
+# print(out[0]['X'][-1])
+# print(out[0]['Y'][-1])
+# print(out[0]['Y'])
+
+def hyperparams_string(inputs):
+    string = ""
+    for key, value in inputs.items():
+        string += f"{key}={value}, "
+    return string[0:-2]
+
+
+# sepcify dict keys that you want the value to be collected
+# it returns a list of tuples of the arugments
+def dicts_collect(keys, dicts):
+    values = []
+    for dict in dicts:
+        values += [[dict[key] for key in keys]]
+    return values
+
+def ploty(inputs, comparing):
+    inp = inputs[0].copy()
+    f_name = inp['f'].name
+    optname = inp['algorithm']
+    
+    del inp[comparing]
+    del inp['f']
+    del inp['X']
+    del inp['Y']
+    inp['algorithm']
+    hs = hyperparams_string(inp)
+
+    title_string = optname + "; " + f_name + "; Varying " + comparing + " \n" + hs
+    
+    fig, ax = plt.subplots()
+    ax.set_ylabel(r'$y(x_i)$')
+    ax.set_xlabel(r'$i$')
+    ax.set_title(title_string)
+    
+    rangei = 50
+    legend_labels = []
+    for (X, Y, var) in dicts_collect(("X", "Y", comparing), inputs):
+        ax.plot(range(len(Y)), Y, linewidth=2.0)
+        legend_labels += [(comparing + ": " + str(var))]
+    ax.legend(legend_labels)
+
+# ploty(out, 'eps')
