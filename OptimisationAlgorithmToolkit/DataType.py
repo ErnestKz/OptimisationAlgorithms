3f60f31b9322b0eb7f9b47087e7a67179d1013c9
ernestkz <ernestkuznecovs@gmail.com>
2022-03-14 13:23:03 +0000

    Checkpoint before refactor.

---
+++ OptimisationAlgorithmToolkit/DataType.py
@@ -0,0 +1,151 @@
+# Each record should contain its label depending on what are the other records in the list.
+
+# The user semi-mannually inputs what the title should be.
+# - Have utility functions to extract pieces of the title from the list of records.
+
+# Function that takes in a list of records.
+#  - For each record determines the label based on what is in the list of records.
+
+# Perhaps there should be a function that calculatesthe meta information that is used by both
+# - utility functions that extract peieces of title
+# - function that assigns the labels to each individual record
+
+
+# MetaInfo: extracts:
+# - Which optimisaiton functions there area
+# - For each optimisation function
+#   - What are the parameters that are not varying and what values do they have
+#   - What are the parameters that are varying and what values do they have
+
+
+
+
+# {
+#   ...
+#   ...
+#   label: 
+# }
+# label made up from what uniquely identifies it
+# - first is optimisation algorithm itself
+# - second are the hyperparmeters that uniqely identifies the cluster of algorithms
+#   - RMSProp alpha0=0.4
+#   - RMSProp alpha0=0.5
+#   - Adam    beta1=0.2  beta2=0.4
+#   - Adam    beta1=0.3  beta2=0.5
+
+# - Then would like to extract the common descriptive pieces
+#   - Different common pieces per algorithm used
+#     - Records -> AlgorihtmName -> CommonThingsString
+#       - Adam: beta1=0.1 eps=0.0001 iters=50 x0=[1, 1]
+#       - RMSProp:  eps=0.0001 iters=50 x0=[1, 1]
+
+
+# MetaRecord extracts
+# - Algorithms and their corresponding Varying fields
+# {
+#   "Adam"    : ["eps", "beta1"]
+#   "RMSProp" : ["eps", "alpha0"]
+# }
+
+
+# meta_record = meta(inputs)
+# inputs = create_labels(meta_record, inputs)
+# inputs = get_title(meta_record, inputs)
+
+# get_titles returns
+# {
+#   "Adam" : "Adam: beta1=0.1 eps=0.0001 iters=50 x0=[1, 1]",
+#   "RMSProp" : "RMSProp:  eps=0.0001 iters=50 x0=[1, 1]"
+# }
+
+import numpy as np
+
+def get_titles(records):
+    m = meta(records)
+    t = {}
+    for alg_name in m.keys():
+        t[alg_name] = get_title(alg_name, records, m)
+    return t
+    
+def get_title(alg_name, records, meta):
+    title = f'{alg_name}:'
+    algs = alg(records, alg_name)
+
+    r = algs[0]
+    params = set(r["algorithm"].all_parameters)
+    varied = meta[alg_name]
+    params.remove('f')
+    params = params - varied
+    
+    for p in params:
+        title += f' {p}={r[p]}'
+    return title
+
+def create_labels(records):
+    m = meta(records)
+    for r in records:
+        r['label'] = create_label(r, m)
+
+# e.g: Adam    beta1=0.2  beta2=0.4
+def create_label(record, meta):
+    alg_name = record['algorithm'].algorithm_name
+    differing_fields = meta[alg_name]
+    label = f'{alg_name}'
+    for f in differing_fields:
+        label += f' {f}={record[f]}'
+    return label
+
+# {
+#   "Adam"    : ["eps", "beta1"]
+#   "RMSProp" : ["eps", "alpha0"]
+# }
+def meta(records):
+    mr = {}
+    algs = get_algs(records)
+    for a in algs:
+        a_records = alg(records, a)
+        mr[a] = differing_fields(a_records)
+    return mr
+
+def differing_fields(records):
+    diff_fields = set({})
+    t = records[0]
+    for r in records:
+        for key, value in r.items():
+            # print("a")
+            # print(t[key])
+            # print(type(value))
+            # print(isinstance(value, list))
+            
+            if isinstance(value, list):
+                value = np.array(value)
+            if isinstance(t[key], list):
+                t[key] = np.array(t[key])
+                
+            b = t[key] == value
+            # print(b)
+            # print(type(b))
+            if type(b) == np.ndarray:
+                b = b.all()
+            if not (b):
+                diff_fields.add(key)
+            
+
+    diff_fields.discard('X')
+    diff_fields.discard('Y')
+    return diff_fields
+
+# extract one algorithm type, filter out the rest
+def alg(records, algorithm_name):
+    return list(filter(lambda r: r['algorithm'].algorithm_name == algorithm_name, records))
+
+# gets algorithms names in the records
+def get_algs(records):
+    algs = set({})
+    for r in records:
+        algs.add(r['algorithm'].algorithm_name)
+    return algs
+    
+    
+# wonder how this would look in haskell
+# funcitonal operators and stuff, would it make it easier.
---
+++ OptimisationAlgorithmToolkit/Plotting.py
@@ -0,0 +1,138 @@
+import matplotlib as mpl
+mpl.rcParams['figure.dpi'] = 200
+mpl.rcParams['figure.facecolor'] = '1'
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-white')
+
+from OptimisationAlgorithmToolkit.DataType import create_labels, get_titles
+
+from matplotlib.ticker import LogLocator
+
+import numpy as np
+
+def plot_contour(records, x1r, x2r, log=False, sym=False):
+    create_labels(records)
+    t = get_titles(records)
+
+    f = records[0]['f']
+    
+    X1, X2 = np.meshgrid(x1r, x2r)
+    Z = np.vectorize(f.function)(X1, X2)
+    if log:
+        plt.contourf(X1, X2, Z, locator=LogLocator(), cmap='RdGy')
+    else:
+        plt.contourf(X1, X2, Z, cmap='RdGy')
+    xlim = plt.xlim()
+    ylim = plt.ylim()
+    for (X, label) in dicts_collect(("X", "label"), records):
+        plt.plot(X.T[0], X.T[1], linewidth=2.0, label=label)
+
+    f = records[0]['f']
+    function_name = f.function_name
+    if sym:
+        f_latex = f.latex()
+        title = rf'${f_latex}$' + " \n " + title_string(records)
+    else:
+        title = title_string(records)
+    plt.xlabel(r'$x_1$')
+    plt.ylabel(r'$x_2$')
+    plt.title(title)
+    
+        
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+    plt.legend()
+    plt.colorbar()
+
+def plot_path(records, xr):
+    create_labels(records)
+    f = records[0]['f'].function;
+    function_name = records[0]['f'].function_name
+    f_latex = records[0]['f'].latex()
+    
+    yr = [f(x) for x in xr]
+    plt.plot(xr, yr)
+    xlim = plt.xlim()
+    ylim = plt.ylim()
+    
+    for (X, label) in dicts_collect(("X", "label"), records):
+        xs = X.flatten()
+        ys = [f(x) for x in xs]
+        plt.plot(xs, ys, linewidth=2.0, label=label)
+
+    plt.xlim(xlim)
+    plt.ylim(ylim)    
+    plt.legend()
+    title = rf'${f_latex}$' + "\n" + title_string(records)
+    plt.title(title)
+    plt.ylabel(f'${function_name}$')
+    plt.xlabel(r'$x$')
+
+def plot_step_size(records, mean=True):
+    create_labels(records)
+    fig, ax = plt.subplots()
+    f_latex = records[0]['f'].latex()
+    for (X, label) in dicts_collect(("X", "label"), records):
+        if mean:
+            s = np.array([np.mean(x) for x in  step_sizes(X).T])
+            ax.plot(np.arange(1, len(s)+1), s, linewidth=2.0, label=label)
+        else:
+            sX = step_sizes(X)
+            for i in range(len(sX)):
+                x = i + 1
+                s = sX[i]
+                ax.plot(np.arange(1, len(s)+1), s, linewidth=2.0, label=label + f' $x_{x} step$')
+    ax.legend()
+
+    title = rf'${f_latex}$' + " \n " + title_string(records)
+    if mean:
+        ax.set_title("Mean Step Across x's \n" + title)
+    else:
+        ax.set_title("Mean Step Across x's \n" + title)
+    ax.set_ylabel(f'Step Size')
+    ax.set_xlabel(r'$i$')
+    
+
+def title_string(records):
+    title = ""
+    t = get_titles(records)
+    for _, v in t.items():
+        title += v + '\n'
+    return title
+    
+# [[x11 x21 x31 ...] [x12 x22 x32 ...] ...]  -> [[x12-x11 x13-x12 ...] [x22-x21 x23-x22 ...] ...]
+def step_sizes(X):
+    return np.array([(x[1:] - x[:-1]) for x in X.T])
+        
+
+
+def ploty(records, sym=False):
+    create_labels(records)
+    t = get_titles(records)
+    
+    fig, ax = plt.subplots()
+    for (X, Y, label) in dicts_collect(("X", "Y", "label"), records):
+        ax.plot(range(len(Y)), Y, linewidth=2.0, label=label)
+
+
+    f = records[0]['f']
+    function_name = f.function_name
+        
+    if sym:
+        f_latex = f.latex()
+        title = rf'${f_latex}$' + " \n " + title_string(records)
+    else:
+        title = title_string(records)
+    
+    ax.set_title(title)
+    ax.set_ylabel(f'${function_name}$')
+    ax.set_xlabel(r'$i$')
+    
+    ax.legend()
+    return ax
+
+def dicts_collect(keys, dicts):
+    values = []
+    for dict in dicts:
+        values += [[dict[key] for key in keys]]
+    return values
